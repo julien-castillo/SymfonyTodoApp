01d039cb715a2fed5dd6f19754219d6cd4a12d3b
CASTILLO Julien <juliencastillo77@gmail.com>
2021-07-11 21:45:06 +0200

    second commit live n° à 52 minutes

--- src/Controller/TodoListController.php
+++ src/Controller/TodoListController.php
@@ -1,1 +1,44 @@
 <?php
+
+namespace App\Controller;
+
+use App\Entity\TodoList;
+use App\Form\TodoListType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class TodoListController extends AbstractController {
+    /**
+     * @Route("/create-list", name="create-list")
+     */
+    public function create(Request $request): Response {
+        $todoList = new TodoList();
+
+        $form = $this->createForm(TodoListType::class, $todoList);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($todoList);
+            $em->flush()
+;        }
+        return $this->render("todo_list/create.html.twig", [
+            "form" => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/", name="create-list")
+     */
+    public function readAll(): Response {
+        $repo = $this->getDoctrine()->getRepository(TodoList::class);
+        $lists = $repo->findAll();
+
+        return $this->render("todo_list/index.html.twig", [
+        "lists" => $lists
+    ]);
+    }
+}
